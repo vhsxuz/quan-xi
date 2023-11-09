635cd55fb0a6b2c740eabf4a5af84a73adc06303
Andreas Alexander <>
2023-11-09 17:17:32 +0700

    feat: create login with github feature

--- client/src/widgets/Navbar.tsx
+++ client/src/widgets/Navbar.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import { Box, Button, Flex, Heading, Spacer } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { Box, Button, Flex, Heading } from '@chakra-ui/react';
 import { FaGithub } from 'react-icons/fa';
 
+
+const CLIENT_ID = '19d575484da3e7969f2a'
+
 const Navbar = () => {
+
+  useEffect(() => {
+    // http://localhost:3000/?code=a440014735f792574eb2
+    const queryString = window.location.search
+    const urlSearchParam = new URLSearchParams(queryString)
+    const codeParam = urlSearchParam.get('code')
+    console.log(codeParam)
+  }, [])
+
+  function loginWithGithub() {
+    window.location.assign('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID)
+  }
+
   return (
     <Box bg="blue.900" p={4}>
       <Flex alignItems="center" justifyContent="space-between">
@@ -12,7 +28,7 @@ const Navbar = () => {
           </Heading>
         </Box>
         <Box>
-          <Button colorScheme="telegram" size="sm" leftIcon={<FaGithub />}>
+          <Button colorScheme="telegram" size="sm" leftIcon={<FaGithub />} onClick={loginWithGithub}>
             Login With Github
           </Button>
         </Box>
